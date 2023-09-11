47733e3039248c911b70309aa0628f047aa33426
Oliver Galante <oliverandcarli@gmail.com>
2023-09-11 16:54:11 -0400

    Changed layout of jscal.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-document.querySelector("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+	console.log("Document Loaded?");
+	const num1 = document.querySelector(".num1");
+	const num2 = document.querySelector(".num2");
+	const num3 = document.querySelector(".num3");
+	const num4 = document.querySelector(".num4");
+	const num5 = document.querySelector(".num5");
+	const num6 = document.querySelector(".num6");
+	const num7 = document.querySelector(".num7");
+	const num8 = document.querySelector(".num8");
+	const num9 = document.querySelector(".num9");
+	const num0 = document.querySelector(".num0");
+	const sum = document.querySelector(".add");
+	const sub = document.querySelector(".subtract");
+	const divide = document.querySelector(".divide");
+	const multiply = document.querySelector(".multiply");
+});
